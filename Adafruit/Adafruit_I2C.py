c8002b2ef341a755b970840a04ce4f4e30a0c66b
John Shovic <jshovic@milocreek.com>
2013-09-08 05:51:20 +0000

    Updated Version of Adafruit_I2C.py

--- Adafruit/Adafruit_I2C.py
+++ Adafruit/Adafruit_I2C.py
@@ -8,9 +8,25 @@ import smbus
 
 class Adafruit_I2C :
 
-  def __init__(self, address, bus=smbus.SMBus(1), debug=False):
+  def getPiRevision():
+    "Gets the version number of the Raspberry Pi board"
+    # Courtesy quick2wire-python-api
+    # https://github.com/quick2wire/quick2wire-python-api
+    try:
+      with open('/proc/cpuinfo','r') as f:
+        for line in f:
+          if line.startswith('Revision'):
+            return 1 if line.rstrip()[-1] in ['1','2'] else 2
+    except:
+      return 0
+ 
+  def __init__(self, address, bus=smbus.SMBus(1 if getPiRevision() > 1 else 0), debug=False):
     self.address = address
+	# By default, the correct I2C bus is auto-detected using /proc/cpuinfo
     self.bus = bus
+	# Alternatively, you can hard-code the bus version below:
+	# self.bus = smbus.SMBus(0); # Force I2C0 (early 256MB Pi's)
+    # self.bus = smbus.SMBus(1); # Force I2C1 (512MB Pi's)
     self.debug = debug
 
   def reverseByteOrder(self, data):
@@ -30,7 +46,7 @@ class Adafruit_I2C :
     try:
       self.bus.write_byte_data(self.address, reg, value)
       if (self.debug):
-        print("I2C: Wrote 0x%02X to register 0x%02X" % (value, reg))
+        print "I2C: Wrote 0x%02X to register 0x%02X" % (value, reg)
     except IOError, err:
       print "Error accessing 0x%02X: Check your I2C address" % self.address
       return -1
@@ -38,7 +54,23 @@ class Adafruit_I2C :
   def writeList(self, reg, list):
     "Writes an array of bytes using I2C format"
     try:
+      if (self.debug):
+        print "I2C: Writing list to register 0x%02X:" % reg
+        print list
       self.bus.write_i2c_block_data(self.address, reg, list)
+    except IOError, err:
+      print "Error accessing 0x%02X: Check your I2C address" % self.address
+      return -1
+
+  def readList(self, reg, length):
+    "Read a list of bytes from the I2C device"
+    results = []
+    try:
+      results = self.bus.read_i2c_block_data(self.address, reg, length)
+      if (self.debug):
+        print "I2C: Device 0x%02X returned the following from reg 0x%02X" % (self.address, reg)
+        print results
+      return results
     except IOError, err:
       print "Error accessing 0x%02X: Check your I2C address" % self.address
       return -1
